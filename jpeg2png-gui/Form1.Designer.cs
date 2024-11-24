fdf63b8f3dd68b631424d1958212aba781eb6c95
Textureman35 <asteamgamer742+noads1@gmail.com>
2024-11-24 10:01:34 +0000

    Add project files.

---
+++ jpeg2png-gui/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace jpeg2png_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 153);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(268, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 87);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 51);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(238, 24);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(397, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 64);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Press Shift to process multiple images.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.progressBar1.Location = new System.Drawing.Point(268, 171);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(250, 31);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 39);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 215);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "jpeg2png-gui";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ jpeg2png-gui/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace jpeg2png_gui
+{
+    public partial class Form1 : Form
+    {
+        List<string> selectedFilePaths = new List<string>();
+        int currentImageIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the label1 default text
+            label1.Text = "Ready";
+
+            // Set dark mode colors for the form and controls
+            this.BackColor = Color.FromArgb(45, 45, 48);
+            this.ForeColor = Color.White;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.BackColor = Color.FromArgb(30, 30, 30);
+
+            button1.BackColor = Color.FromArgb(28, 28, 28);
+            button1.ForeColor = Color.White;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(64, 64, 64);
+
+            button2.BackColor = Color.FromArgb(28, 28, 28);
+            button2.ForeColor = Color.White;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderColor = Color.FromArgb(64, 64, 64);
+            button2.MouseEnter += Button2_MouseEnter;
+            button2.MouseLeave += Button2_MouseLeave;
+
+            comboBox1.BackColor = Color.FromArgb(28, 28, 28);
+            comboBox1.ForeColor = Color.White;
+            comboBox1.DrawMode = DrawMode.OwnerDrawFixed;
+            comboBox1.DrawItem += ComboBox_DrawItem;
+            comboBox1.Items.AddRange(new object[] {
+                "Select Weight",
+                "0.0",
+                "0.1",
+                "0.2",
+                "0.3",
+                "0.4",
+                "0.5 [RECOMMENDED]",
+                "0.6",
+                "0.7",
+                "0.8",
+                "0.9",
+                "1.0"
+            });
+            comboBox1.SelectedIndex = 0;
+
+            comboBox2.BackColor = Color.FromArgb(28, 28, 28);
+            comboBox2.ForeColor = Color.White;
+            comboBox2.DrawMode = DrawMode.OwnerDrawFixed;
+            comboBox2.DrawItem += ComboBox_DrawItem;
+            comboBox2.Items.AddRange(new object[] {
+                "Select Iterations",
+                "25",
+                "50",
+                "75",
+                "100 [RECOMMENDED]",
+                "125",
+                "150",
+                "175",
+                "200"
+            });
+            comboBox2.SelectedIndex = 0;
+
+            groupBox1.BackColor = Color.FromArgb(45, 45, 48);
+            groupBox1.ForeColor = Color.White;
+
+        }
+
+        private void ComboBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            e.DrawBackground();
+            e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(28, 28, 28)), e.Bounds);
+
+            if (e.Index >= 0)
+            {
+                string text = comboBox.Items[e.Index].ToString();
+                e.Graphics.DrawString(text, e.Font, new SolidBrush(Color.White), e.Bounds);
+            }
+            e.DrawFocusRectangle();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select JPEG files",
+                Filter = "JPEG Files (*.jpeg;*.jpg)|*.jpeg;*.jpg",
+                Multiselect = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePaths = openFileDialog.FileNames.ToList();
+                DisplayImage(0);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (selectedFilePaths.Count == 0)
+            {
+                MessageBox.Show("Please select files first.");
+                return;
+            }
+
+            if (comboBox1.SelectedIndex == 0 || comboBox2.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select valid options for both weight and iterations.");
+                return;
+            }
+
+            // Set label text to "Processing" before starting the processing
+            label1.Text = "Processing";
+            progressBar1.Value = 0; // Reset progress bar
+
+            string weightText = comboBox1.SelectedItem.ToString().Split(' ')[0];
+            string iterationsText = comboBox2.SelectedItem.ToString().Split(' ')[0];
+
+            float weight = float.Parse(weightText);
+            int iterations = int.Parse(iterationsText);
+
+            int completed = 0;
+            foreach (string filePath in selectedFilePaths)
+            {
+                string outputFilePath = Path.ChangeExtension(filePath, ".png");
+
+                if ((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog
+                    {
+                        Filter = "PNG Files (*.png)|*.png",
+                        DefaultExt = "png",
+                        FileName = outputFilePath
+                    };
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        outputFilePath = saveFileDialog.FileName;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                string arguments = $"-o \"{outputFilePath}\" -w {weight} -i {iterations} \"{filePath}\"";
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "jpeg2png.exe",
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                try
+                {
+                    using (Process process = Process.Start(startInfo))
+                    {
+                        process.WaitForExit();
+                    }
+                    completed++;
+                    this.Text = $"Processing files... [{completed}/{selectedFilePaths.Count}]";
+                    progressBar1.Value = (completed * 100) / selectedFilePaths.Count;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while processing the file: " + ex.Message);
+                }
+            }
+
+            MessageBox.Show("Processing complete. All output files are saved.");
+            label1.Text = "Ready"; // Revert label1 text to "Ready"
+            this.Text = "jpeg2png GUI";
+            progressBar1.Value = 100; // Ensure progress bar is full at completion
+        }
+
+        private void DisplayImage(int index)
+        {
+            if (index >= 0 && index < selectedFilePaths.Count)
+            {
+                try
+                {
+                    pictureBox1.Image = Image.FromFile(selectedFilePaths[index]);
+                    currentImageIndex = index;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred while loading the image: " + ex.Message);
+                }
+            }
+        }
+
+        private void Button2_MouseEnter(object sender, EventArgs e)
+        {
+            label1.Text = "Press Shift to set output location."; // Update label1 text on hover for button2
+        }
+
+        private void Button2_MouseLeave(object sender, EventArgs e)
+        {
+            label1.Text = "Ready"; // Revert label1 text when not hovering over button2
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e) { }
+
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        private void progressBar1_Click(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
